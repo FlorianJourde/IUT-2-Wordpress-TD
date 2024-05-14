42639d2c331026cb5d66e158d676eb7d83094b7b
Florian Jourde <florian.jourde.aff@unilim.fr>
2024-05-14 11:01:31 +0200

    Add plugins

---
+++ plugins/akeebabackupwp/helpers/private/wp-config.php
@@ -0,0 +1,7 @@
+<?php
+global $table_prefix;
+$table_prefix = 'wp_';
+define('DB_NAME', 'u718790758_auto87_db');
+define('DB_USER', 'u718790758_auto87_user');
+define('DB_PASSWORD', '4a^MPaN0|');
+define('DB_HOST', 'localhost');
